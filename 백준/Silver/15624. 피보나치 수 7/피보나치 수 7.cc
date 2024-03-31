d44430b36dfbd3e93f001af7a78b1442ff088271
YoungWoo Yang <119781151+0u-Y@users.noreply.github.com>
2024-03-31 22:05:33 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 8 ms, Memory: 5928 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
@@ -4,22 +4,20 @@ using namespace std;
 
 int n;
 
-int dp[1000001];
+int dp[1000001] = {0,1};
 
 int main(){
 	cin>>n;
-
-	dp[0] = 0;
-	dp[1] = 1;
-	dp[2] = 1;
 	
 
-	for(int i=3; i<=n; i++){
-		dp[n] = dp[n-1] + dp[n-2];
+
+
+	for(int i=2; i<=n; i++){
+		dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
 		
 	}
 
-	cout<<dp[n] % 1000000007;
+	cout<<dp[n];
 
 
 }
