019cd4f8956a2377572b79dc8b623c904dde4858
YoungWoo Yang <119781151+0u-Y@users.noreply.github.com>
2024-04-07 01:21:24 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
+        String[] input = bf.readLine().split(" ");
+
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+
+
+        System.out.print(a-b);
+
+    }
+}
+
+
+
+
+
