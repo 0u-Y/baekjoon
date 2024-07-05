c14d8b01edb6f25fe3cfdb2c87e2e6f3e18433c2
YoungWoo Yang <119781151+0u-Y@users.noreply.github.com>
2024-07-05 21:16:14 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 3956 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,46 +1,64 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
+
 
 
 using namespace std;
 
+int deg[32001];
 
-int main() {
-    ios_base::sync_with_stdio(false);
+vector<int> adj[32001];
+
+
+
+
+
+
+
+int main(){
+
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
     int n,m;
     cin>>n>>m;
-    int a,b;
-    vector<int> adj[32001];
-
-    int deg[32001];
 
     for(int i=0; i<m; i++){
+        int a,b;
         cin>>a>>b;
         adj[a].push_back(b);
         deg[b]++;
     }
+
     queue<int> Q;
 
-    for(int i  = 1; i<=n; i++){
+    for(int i=1; i<=n; i++){
         if(deg[i] == 0){
             Q.push(i);
         }
     }
+
     while(!Q.empty()){
         int cur = Q.front(); Q.pop();
         cout<<cur<<" ";
-        for(int i=0; i<adj[cur].size(); i++){
-            int nxt = adj[cur][i];
+        for(int nxt : adj[cur]){
             deg[nxt]--;
-            if(deg[nxt] == 0){
-                Q.push(nxt);
-            }
+            if(deg[nxt] == 0) Q.push(nxt);
         }
-
-
     }
-   
+
+    
+
+
+
+
+
+
+
+
+
+
+
 }
